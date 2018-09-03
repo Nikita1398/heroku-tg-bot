eed73426d672f91127561246f1a31704c5b45e50
Nikita1398 <Sorokin13988@gmail.com>
2018-09-03 17:28:19 +0400

    initial

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ const TOKEN = '672415727:AAHNvm7e62b5BxtR1Qw62SwlszD58H0zF-g'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Hello from HEROKU, bot says: "Hi, ${msg.from.first_name}"`)
+    bot.sendMessage(msg.chat.id, `Привет: "Hi, ${msg.from.first_name}"`)
 })
+require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+    res.end('')})
